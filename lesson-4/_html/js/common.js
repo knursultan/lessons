8bc29d1177ff3867359bc8816be67e5c16d85dd1
knursultan <knursultana@gmail.com>
2017-07-10 11:26:16 +0600

    add

--- lesson-4/_html/js/common.js
+++ lesson-4/_html/js/common.js
@@ -1,5 +1,16 @@
 $(function() {
+	function initSize(){
+		$(".till_item .tc").each(function(){
+			var parh = $(this).parent().height();
+			$(this).height(parh);
+		});
+	}
 
+	initSize();
+
+	$(window).resize(function(){
+		initSize();
+	});
 	//SVG Fallback
 	if(!Modernizr.svg) {
 		$("img[src*='svg']").attr("src", function() {
