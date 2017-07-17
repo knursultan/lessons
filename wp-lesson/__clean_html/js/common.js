bed784d346c49f26bbf824f64c77f0e41ef92c72
knursultan <knursultana@gmail.com>
2017-07-17 17:24:03 +0600

    second

--- wp-lesson/__clean_html/js/common.js
+++ wp-lesson/__clean_html/js/common.js
@@ -1,5 +1,28 @@
 $(document).ready(function() {
+    function heightDetect() {
+        $(".main_head").css("height", $(window).height());
+    };
 
-	
-	
+    heightDetect();
+
+    $(window).resize(function() {
+        heightDetect();
+    });
+
+    $(".toogle_menu, .menu_item").click(function() {
+        $(".sandwich").toggleClass("active");
+    });
+
+    $(".toogle_menu").click(function() {
+    	if ($(".top_menu").is(":visible")){
+	    	$(".top_menu").fadeOut(600);
+    	} else {
+	    	$(".top_menu").fadeIn(600);
+    	}
+    });
+});
+
+$(window).load(function() {
+    $(".loader_inner").fadeOut();
+    $(".loader").delay(400).fadeOut("slow");
 });
