42ccba6f5a46ffcd20544727c00df7b21cad6786
knursultan <knursultana@gmail.com>
2017-07-21 18:26:25 +0600

    seven

--- wp-lesson/__clean_html/js/common.js
+++ wp-lesson/__clean_html/js/common.js
@@ -4,9 +4,12 @@ $(document).ready(function() {
 	$(".top_text h1").animated("fadeInDown", "fadeOutUp");
 	$(".top_text p, .section_header").animated("fadeInUp", "fadeOutDown");
 
-	$(".animation_1").animated("flipInY", "flipOutY");
-	$(".animation_2").animated("fadeInLeft", "fadeOutLeft");
-	$(".animation_3").animated("fadeInRight", "fadeOutRight");
+	$(".animation_1").animated("flipInY", "flipOutDown");
+	$(".animation_2").animated("fadeInLeft", "fadeOutDown");
+	$(".animation_3").animated("fadeInRight", "fadeOutDown");
+
+	$(".left .resume_item").animated("fadeInLeft", "fadeOutDown");
+	$(".right .resume_item").animated("fadeInRight", "fadeOutDown");
 
     function heightDetect() {
         $(".main_head").css("height", $(window).height());
