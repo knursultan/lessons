fe5452aff1043c326f4814110b506975398cb034
knursultan <knursultana@gmail.com>
2017-08-15 14:12:25 +0600

    finish

--- lesson-5/assets/js/functions.js
+++ lesson-5/assets/js/functions.js
@@ -1,12 +1,30 @@
-(function ($) {
-  'use strict';
+(function($) {
+    'use strict';
 
-  $(document).ready(function () {
+    function blueasyTabs() {
+        var $wrapper = $('.tab-wrapper'),
+            $menu = $wrapper.find('.tab-menu li'),
+            $content = $wrapper.find('.thumbs-row');
 
+        $content.not(':first-of-type').hide();
+        $content.each(function(i) {
+            $(this).attr('data-tab', 'tab-' + i);
+        });
+        $menu.each(function(i) {
+            $(this).attr('data-tab', 'tab-' + i);
+        });
 
+        $menu.click(function() {
+            var $getWrapper = $(this).closest($wrapper),
+                dataTab = $(this).attr('data-tab');
 
+            $getWrapper.find($menu).find('a').removeClass('active');
+            $(this).find('a').addClass('active');
 
-
-  }); //end ready
+            $getWrapper.find($content).hide();
+            $getWrapper.find($content).filter('[data-tab=' + dataTab + ']').show();
+        });
+    }
+    blueasyTabs();
 
 }(jQuery));
