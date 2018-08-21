9771e743c372ef04840f1525bad0b8b602511d62
Нурсултан <nursultan@MacBook-Air-Nursultan.local>
2018-08-21 22:26:31 +0600

    promise

---
+++ ES6/dist/promises.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var movieList = document.getElementById('movies');
+
+function addMovieToList(movie) {
+    var img = document.createElement('img');
+    img.src = movie.Poster;
+    movieList.appendChild(img);
+}
+
+function getData(url) {
+    return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                var json = JSON.parse(xhr.response);
+                resolve(json.Search);
+            } else {
+                reject(xhr.statusText);
+            }
+        };
+
+        xhr.onerror = function (error) {
+            reject(error);
+        };
+
+        xhr.send();
+    });
+}
+
+var batman = getData('http://www.omdbapi.com/?apikey=730993d6&s=batman');
+var superman = getData('http://www.omdbapi.com/?apikey=730993d6&s=superman');
+
+batman.then(function (movies) {
+    return movies.forEach(function (movie) {
+        return addMovieToList(movie);
+    });
+}).catch(function (error) {
+    return console.error(error);
+});
+
+superman.then(function (movies) {
+    return movies.forEach(function (movie) {
+        return addMovieToList(movie);
+    });
+}).catch(function (error) {
+    return console.error(error);
+});
+
+// Promise.race([batman, superman])
+//     .then(movies =>
+//         movies.forEach(movie =>
+//             addMovieToList(movie)))
+//     .catch(error => console.error(error));
